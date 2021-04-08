030ffee07f6f02c4141ce7f3899246ced545ae2d
José Fredes <76954215+JoseFredes@users.noreply.github.com>
2021-04-08 16:11:17 -0400

    2

---
+++ swish.pl
@@ -0,0 +1,17 @@
+auto(opelastra, tiene1600cc).
+auto(opelastra, tieneairbag). 
+auto(opelastra, notieneabs). 
+auto(fiat, tiene1400cc). 
+auto(fiat, notieneairbag). 
+auto(fiat, notieneabs). 
+auto(renaultspace, monovolumen). 
+auto(renaultspace, motordiesel). 
+auto(renaultspace, tieneabs). 
+auto(renaultspace, tieneairbag). 
+
+
+cilindradabaja(X,tiene1400cc):- auto(X,tiene1400cc).
+cilindradamedia(X,tiene1600cc):- auto(X,tiene1600cc).
+seguridadalta(Z,tieneairbag,tieneabs):-auto(Z,tieneabs),auto(Z,tieneairbag).
+
+
